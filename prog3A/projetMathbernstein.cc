49d2244798832f36116f6c8baa5428e060e0953a
Florian Fizaine <50233689+flofiz@users.noreply.github.com>
2020-01-10 23:49:19 +0100

    Add files via upload

---
+++ prog3A/projetMathbernstein.cc
@@ -0,0 +1,20 @@
+/********************************************************/
+/*                    projetMathbernstein.cc                          */
+/********************************************************/
+/*                                                       */
+/********************************************************/ 
+
+
+#include <stdlib.h>
+#define Pi 3.141592654
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+int main(int argc,char **argv)
+{
+    cout<<"Bonjour et bon travail"<<endl;
+    return 0;
+}
